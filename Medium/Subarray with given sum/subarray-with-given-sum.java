4512a15c4340633d0fa083ea7b9d7aa85eef6789
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2023-12-21 11:57:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarray with given sum/subarray-with-given-sum.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> h=new ArrayList<Integer>();
+        int left=0;
+        int right=0;
+        int sum=0;
+        
+       while(right<n){
+           sum+=arr[right];
+            
+       while(sum>s && left<=right){
+           sum-=arr[left];
+           left++;
+            }
+         
+       if(sum==s && left<=right){
+           h.add(left+1);
+           h.add(right+1);
+           return h;
+            }
+            
+        right++;
+        }
+        h.add(-1);
+        return h;
+    }
+}
