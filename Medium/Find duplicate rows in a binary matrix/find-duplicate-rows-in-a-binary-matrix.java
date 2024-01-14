937ed7e8c126d21f5df437e6ef204fecceef9b5e
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-01-14 09:17:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find duplicate rows in a binary matrix/find-duplicate-rows-in-a-binary-matrix.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    arr[i][j] = Integer.parseInt(s[j]);
+                }
+            }
+            ArrayList<Integer> ans = new Solution().repeatedRows(arr, n, m);
+            for(int i=0;i<ans.size();i++)
+                System.out.print(ans.get(i)+" ");
+                
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n)
+    {
+        //code here
+        ArrayList<Integer>list=new ArrayList<>();
+        HashSet<String>h=new HashSet<>();
+        for(int i=0;i<m;i++){
+            String num="";
+            for(int j=0;j<n;j++){
+                 num=num+(char)(matrix[i][j]+'0');
+            }
+            if(h.contains(num)){
+                list.add(i);
+            }
+            else{
+                h.add(num);
+            }
+        }
+        return list;
+    }
+}
