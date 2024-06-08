7a4f37ccd190fdb3651a975ce42982e1ea5d1f0b
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-06-08 11:36:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Index of an Extra Element/index-of-an-extra-element.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class ExtraElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n - 1];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for (int i = 0; i < n - 1; i++) b[i] = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.findExtra(n, a, b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class Solution {
+    public int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr2.length;i++){
+            map.put(arr2[i],i);
+        }
+        for(int i=0;i<arr1.length;i++){
+            if(!map.containsKey(arr1[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
